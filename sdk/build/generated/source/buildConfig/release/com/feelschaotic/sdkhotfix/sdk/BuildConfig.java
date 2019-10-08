5d2fccd962eaf839c31391c30ae7122e14bd2c61
FeelsChaotic <feelschaotic@gmail.com>
2019-10-08 10:28:35 +0800

    untracked files on master: aae1dfa docs: 修改README的改进说明项

---
+++ sdk/build/generated/source/buildConfig/release/com/feelschaotic/sdkhotfix/sdk/BuildConfig.java
@@ -0,0 +1,21 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.feelschaotic.sdkhotfix.sdk;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.feelschaotic.sdkhotfix.sdk";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  // Fields from build type: release
+  public static final String ACCESS_ID = "LTAI4Fx9ecT1eH86xzRKt2Yc";
+  public static final String BMOB_APPLICATION_ID = "6858e5a9b06a8e62b2ffdfc1ce83b843";
+  public static final String BUCK_NAME = "sdkhotfix-app";
+  public static final String END_POINT = "https://oss-cn-shenzhen.aliyuncs.com";
+  public static final String HOTFIX_SERVER_URL = "https://api.bmob.cn/1/functions/";
+  public static final String REST_API_KEY = "eea64ff2521260acbae5305889a00ed4";
+  public static final String SECRET_KEY = "JtMMOLEtwxTZe31soVgKGpVbwovUP8";
+}
