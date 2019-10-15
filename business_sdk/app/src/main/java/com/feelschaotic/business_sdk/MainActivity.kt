c0fd8d94750d6c2fb42e754d4765d08847d05069
FeelsChaotic <feelschaotic@gmail.com>
2019-10-15 11:50:38 +0800

    feat: 完善接入示例项目的功能，修正拼写和配置

--- business_sdk/app/src/main/java/com/feelschaotic/business_sdk/MainActivity.kt
+++ business_sdk/app/src/main/java/com/feelschaotic/business_sdk/MainActivity.kt
@@ -2,6 +2,9 @@ package com.feelschaotic.business_sdk
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import com.feelschaotic.samplesdk.SdkManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,18 +12,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-       /* init_btn.setOnClickListener {
+        init_btn.setOnClickListener {
             Log.d("TAG", "初始化业务sdk")
             SdkManager.init(application)
         }
 
         fix_btn.setOnClickListener {
-            Log.d("TAG", "初始化hotfix sdk（延迟初始化，避免一进来就初始化会把补丁给打上，看不到效果）")
+            Log.d("TAG", "初始化hotfix sdk")
             SdkManager.fixBug()
         }
+
         call_bug_btn.setOnClickListener {
             Log.d("TAG", "调用业务sdk有bug的方法")
             SdkManager.callBug()
-        }*/
+        }
+
+        call_bug_again_btn.setOnClickListener {
+            Log.d("TAG", "调用业务sdk有bug的方法")
+            SdkManager.callBug()
+        }
     }
 }
--- business_sdk/samplesdk/src/main/java/com/feelschaotic/samplesdk/SdkManager.kt
+++ business_sdk/samplesdk/src/main/java/com/feelschaotic/samplesdk/SdkManager.kt
@@ -2,6 +2,10 @@ package com.feelschaotic.samplesdk
 
 import android.app.Application
 import android.util.Log
+import com.feelschaotic.sdkhotfix.sdk.HotfixConfig
+import com.feelschaotic.sdkhotfix.sdk.HotfixManager
+import com.feelschaotic.sdkhotfix.sdk.statistics.StatisticsListener
+import com.feelschaotic.sdkhotfix.sdk.utils.LogUtils
 import com.meituan.robust.patch.annotaion.Modify
 import java.lang.ref.WeakReference
 import java.util.*
@@ -16,7 +20,6 @@ object SdkManager {
 
     fun init(application: Application) {
         applicationRef = WeakReference(application)
-
     }
 
     fun fixBug() {
@@ -24,7 +27,7 @@ object SdkManager {
 
         applicationRef?.get()?.let {
             val hotfixConfig = HotfixConfig.Builder()
-                .debug(BuildConfig.DEBUG)//为true开启内部log打印
+                .debug(true)//为true开启内部log打印
                 .packageName(BuildConfig.APPLICATION_ID + "_" + BuildConfig.SDK_ARTIFACT_ID)//视情况加上ARTIFACT_ID 有时候会出现业务方sdk需要提供多ARTIFACT_ID的情况
                 .appVersion(BuildConfig.SDK_VERSION)
                 .statisticsListener(object : StatisticsListener {
@@ -37,7 +40,15 @@ object SdkManager {
         }
     }
 
+    @Modify
     fun callBug() {
+        try {
             var i = 1 / 0
+            LogUtils.d("TAG", "一行代码不会被插桩，所以加个log增加点代码量~~~~~~")
+            LogUtils.d("TAG", "一行代码不会被插桩，所以加个log增加点代码量~~~~~~")
+        } catch (e: Exception) {
+            LogUtils.printError(e)
+        }
+
     }
 }
